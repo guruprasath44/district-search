8ace75582e4dbb75c876525858ac4e4c257cb40d
Guruprasath4401 <guruprasathindirakumar@gmail.com>
2021-06-09 17:55:58 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'district-search';
+  districtName = new FormControl('');
+  area = new FormControl('');
+  population = new FormControl('');
+  districts: { value: string; display: string; }[] = [];
+   constructor(){
+      this.loadDistricts();
+   }
+
+   loadDistricts() {
+      var allDistricts = 'Ariyalur, Chengalpattu, Chennai, Coimbatore, Cuddalore, Dharmapuri, Dindigul, Erode, Kallakurichi,\
+      Kanchipuram, Kanyakumari, Karur, Krishnagiri, Madurai, Nagapattinam, Namakkal, Nilgiris, Perambalur, Pudukkottai,\
+      Ramanathapuram, Ranipet, Salem, Sivaganga, Tenkasi, Thanjavur, Theni, Thoothukudi (Tuticorin), Tiruchirappalli,\
+       Tirunelveli, Tirupathur, Tiruppur, Tiruvallur, Tiruvannamalai, Tiruvarur, Vellore, Viluppuram, Virudhunagar' ;
+      this.districts =  allDistricts.split(/, +/g).map( function (district) {
+         return {
+            value: district.toUpperCase(),
+            display: district
+         };
+      });
+   }
+
 }
